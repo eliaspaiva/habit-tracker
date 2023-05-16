d765849d107d512f0232b8bc2064a4a35d63a1c6
Elias Paiva <elias.jpaiva@gmail.com>
2023-05-16 20:35:01 +0200

    Updating Repo

---
+++ App.js
@@ -0,0 +1,32 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", null, props.name),
+    React.createElement("h2", null, props.animal),
+    React.createElement("h2", null, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      animal: "dog",
+      name: "James",
+      breed: "bacet d",
+    }),
+    React.createElement(Pet, {
+      animal: "cat",
+      name: "Luna",
+      breed: "mis",
+    }),
+    React.createElement(Pet, {
+      animal: "hamester",
+      name: "frufru",
+      breed: "rat",
+    }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
